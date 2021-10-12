175961fe295f887d1e05c6b07c66c02cf749160a
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2021-10-12 14:56:29 +0200

    v1 release

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const repo_token = core.getInput('repo-token');
+    const context = github.context;
+    const octokit = github.getOctokit(repo_token);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const { data: issues } = await octokit.rest.issues.listForRepo({
+        ...context.repo,
+        state: 'open',
+        sort: 'created',
+        direction: 'asc',
+        per_page: 100
+    }).then(issues => {
+      core.info(`Testing ${issues} ...`);
+      // issues is an array of all issue objects
+    });
 
-    core.setOutput('time', new Date().toTimeString());
+    // const ms = core.getInput('milliseconds');
+
+
+    // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // await wait(parseInt(ms));
+    // core.info((new Date()).toTimeString());
+
+    // core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
