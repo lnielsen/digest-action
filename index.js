be1c2650a6d75381582cea840b2e5805ab06a002
Jenny Bonsak <jenny.bonsak@gmail.com>
2021-11-01 17:06:12 +0100

    Log issue details to Discord

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 async function run() {
   try {
     const repo_token = core.getInput('repo-token');
     const context = github.context;
     const octokit = github.getOctokit(repo_token);
+    const WEBHOOK_ID = core.getInput('webhook-id') || '';
+    const WEBHOOK_TOKEN = core.getInput('webhook-token') || '';
 
-    const { data: issues } = await octokit.rest.issues.listForRepo({
+    if(WEBHOOK_ID === '' || WEBHOOK_TOKEN === '') {
+      console.log("Cannot find webhook values");
+    } 
+    else {
+      const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
         ...context.repo,
         state: 'open',
         sort: 'created',
         direction: 'asc',
-        per_page: 100
-    }).then(issues => {
-      core.info(`Testing ${issues} ...`);
-      // issues is an array of all issue objects
-    });
+        per_page: 100,
+      });
+      
+      let content = '';
 
-    // const ms = core.getInput('milliseconds');
+      for await (const { data: issues } of iterator) {
+        for (const issue of issues) {
+          let issueMsg = `**${issue.title}**\nby ${issue.user.login} - <${issue.html_url}>\n\n`;
+          console.log(issueMsg);
+          content += issueMsg;
+        }
+      }
 
+      axios.post(`https://discord.com/api/webhooks/${WEBHOOK_ID}/${WEBHOOK_TOKEN}`, { content })
+      .then(() => console.log("Message sent to Discord"))
+      .catch(error => console.log("Error sending message to Discord:", error.message))
 
-    // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    // await wait(parseInt(ms));
-    // core.info((new Date()).toTimeString());
-
-    // core.setOutput('time', new Date().toTimeString());
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
